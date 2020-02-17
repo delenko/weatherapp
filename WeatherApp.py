d0e9465dcbd17129d46f5d3ff3276bcc4cc55f44
delenko <cobramustang13@hotmail.com>
2020-02-16 22:56:10 -0700

    removed button. added line edit

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,42 +1,62 @@
 import sys
 import random
 import requests
-from PySide2 import QtCore, QtWidgets,QtGui
+from PySide2.QtWidgets import QWidget,QVBoxLayout, QPushButton, QLineEdit, QLabel, QGridLayout,QApplication, QGroupBox
+from PySide2 import QtCore, QtGui
 
-class MyWidget(QtWidgets.QWidget):
+
+
+class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.textfield = QtWidgets.QLineEdit("")
-        self.button = QtWidgets.QPushButton("Enter")
-        self.text = QtWidgets.QLabel("Boo")
-        self.layout = QtWidgets.QVBoxLayout()
-        self.text.setAlignment(QtCore.Qt.AlignCenter)
-        self.layout.addWidget(self.textfield)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-        self.setLayout(self.layout)
-        self.button.clicked.connect(self.weather())
+        self.createLayout()
+        vboxes = QVBoxLayout()
+        vboxes.addWidget(self.gb)
+        self.setLayout(vboxes)
 
-    def weather(self):
+    def createLayout(self):
+        self.gb = QGroupBox("Enter your zip code.")
+        self.gb.setAlignment(QtCore.Qt.AlignCenter)
+        self.vbox = QVBoxLayout()
+        self.label = QLabel("Your Zip Code")
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        textfield = QLineEdit("")
+        textfield.setAlignment(QtCore.Qt.AlignCenter)
+        self.vbox.addWidget(textfield)
+        self.vbox.addWidget(self.label)
+        textfield.textChanged[str].connect(self.weather)
+        self.gb.setLayout(self.vbox)
+
+    def weather(self,text):
         params = {
             'access_key':'d7ed3db83b545d1235bdd8008de97b13',
             'query': '',
             'unit':'f'
         }
-        params['query'] = self.textfield.text()
-        api_result = requests.get('http://api.weatherstack.com/current', params)
-        response = api_result.json()
+        params['query'] = text
+        params['unit'] = 'f'
+        self.label.setText(text)
+        print(params) 
+        if params['query']!= "":
+            api_result = requests.get('http://api.weatherstack.com/current', params)
+            response = api_result.json()
+            temperature = response['current']['temperature']
+            self.label.setText(u"Current temperature of zip code is %d F" % (((response['current']['temperature']*9)/5)+32))
+        else:
+            return
+        
         print(response)
-        #temperature = response['current']['temperature']
-        #self.textfield.setText(u"Current temperature of zip code is %d" % (response['list']['3']['current']['temperature']))
+      
 
 
-if __name__ =="__main__":
-    app = QtWidgets.QApplication([])
 
-    widget = MyWidget()
-    widget.resize(800,600)
-    widget.show()
+app = QApplication(sys.argv)
 
-    sys.exit(app.exec_())
+widget = MyWidget()
+widget.resize(400,400)
+widget.setWindowIconText("Weather")
+widget.setWindowTitle("Weather")
+widget.show()
+app.exec_()
+sys.exit()
